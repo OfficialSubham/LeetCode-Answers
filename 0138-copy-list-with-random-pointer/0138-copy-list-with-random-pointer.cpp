c226a0b7488a6d492ab5ba3ac87bbe1161ebb74d
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-11-14 19:57:53 +0530

    Time: 10 ms (20.05%), Space: 15 MB (86.58%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,50 +17,87 @@ public:
 class Solution {
 public:
 
-    int getIndexOfNode(Node* head, Node* random) {
-        int cnt = 0;
-        while(head) {
-            cnt++;
-            if(head == random) return cnt;
-            head = head->next;
-        } 
-        return cnt;
-    }
+    // int getIndexOfNode(Node* head, Node* random) {
+    //     int cnt = 0;
+    //     while(head) {
+    //         cnt++;
+    //         if(head == random) return cnt;
+    //         head = head->next;
+    //     } 
+    //     return cnt;
+    // }
     
-    Node* getNodeAtIndex(Node* head, int n) {
-        int cnt = 0;
-        while(head) {
-            cnt++;
-            if(cnt == n) return head;
-            head = head->next;
-        }
-        return nullptr;
-    }
+    // Node* getNodeAtIndex(Node* head, int n) {
+    //     int cnt = 0;
+    //     while(head) {
+    //         cnt++;
+    //         if(cnt == n) return head;
+    //         head = head->next;
+    //     }
+    //     return nullptr;
+    // }
+
+    // Node* copyRandomList(Node* head) {
+    //     if(!head) return head;
+    //     Node* copiedHead = new Node(head->val);
+    //     Node* mover = head->next;
+    //     Node* tail = copiedHead;
+
+    //     while(mover) {
+    //         Node* tempNode = new Node(mover->val);
+    //         tail->next = tempNode;
+    //         tail = tail->next;
+    //         mover = mover->next;
+    //     }
+    //     mover = head;
+    //     Node* copiedMover = copiedHead;
+    //     while(mover) {
+    //         if(mover->random) {
+    //             int nthNode = getIndexOfNode(head, mover->random);
+    //             Node* randomNode = getNodeAtIndex(copiedHead, nthNode);
+    //             copiedMover->random = randomNode;
+    //         }
+    //         mover = mover->next;
+    //         copiedMover = copiedMover->next;
+    //     }
+
+    //     return copiedHead;
+    // }
 
     Node* copyRandomList(Node* head) {
         if(!head) return head;
-        Node* copiedHead = new Node(head->val);
-        Node* mover = head->next;
-        Node* tail = copiedHead;
-
-        while(mover) {
-            Node* tempNode = new Node(mover->val);
-            tail->next = tempNode;
-            tail = tail->next;
-            mover = mover->next;
+        Node* original = head;
+        while(original){
+            Node* tempNode = new Node(original->val);
+            Node* next = original->next;
+            original->next = tempNode;
+            original = next;
+            tempNode->next = original;
         }
-        mover = head;
-        Node* copiedMover = copiedHead;
-        while(mover) {
-            if(mover->random) {
-                int nthNode = getIndexOfNode(head, mover->random);
-                Node* randomNode = getNodeAtIndex(copiedHead, nthNode);
-                copiedMover->random = randomNode;
+
+        original = head;
+
+        Node* curr = head;
+        while(curr) {
+            if(curr->random && !curr->next->random) {
+                curr->next->random = curr->random ? curr->random->next : nullptr;
             }
-            mover = mover->next;
-            copiedMover = copiedMover->next;
+            curr = curr->next->next;
         }
 
-        return copiedHead;
+        Node* currHead = head;
+        Node* cloneHead = head->next;
+
+        while(currHead) {
+            Node* clone = currHead->next;
+            currHead->next = clone->next;
+            if(clone->next) clone->next = clone->next->next;
+            currHead = currHead->next;
+        }
+
+        return cloneHead;
+
+        
     }
+
 };
