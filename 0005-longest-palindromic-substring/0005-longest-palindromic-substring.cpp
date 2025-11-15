baf09f764fc2a3f248c025fed41721e0fbf14b14
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-11-15 16:08:06 +0530

    Time: 4 ms (93.24%), Space: 9.4 MB (80.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,9 @@
 class Solution {
 public:
-    // string longestPalindrome(string s) {
-    //     int n = s.size();
-    //     string ans = "";
-    //     for(int i = 0; i < s.size(); i++) {
-    //         int j = i;
-    //         int k = i;
-    //         while(j >= 0 && k < n && s[j] == s[k]) {
-    //             j--;
-    //             k++;
-    //         }
-    //         if(ans.size() < (k - j - 1)) {
-    //             ans = s.substr(j + 1, k - j - 1);
-    //         }
-
-    //         int l = i;
-    //         int m = i+1;
-    //         while(l >= 0 && m < n && s[l] == s[m]) {
-    //             l--;
-    //             m++;
-    //         }
-    //         if(ans.size() < (m - l - 1)) {
-    //             ans = s.substr(l + 1, m - l - 1);
-    //         }
-
-    //     }
-    //     return ans;
-
-    // }
-
     string longestPalindrome(string s) {
         int n = s.size();
-        string ans = "";
+        int start = 0;
+        int end = 1;
         for(int i = 0; i < n; i++) {
             int j = i;
             int k = i;
@@ -39,8 +11,9 @@ public:
                 j--;
                 k++;
             }
-            if(ans.size() < (k - j - 1)){
-                ans = s.substr(j + 1, (k - j - 1));
+            if((end - start) < (k - j - 1)){
+                end = k;
+                start = j + 1;
             }
 
             int l = i;
@@ -50,12 +23,14 @@ public:
                 m++;
             }
 
-            if(ans.size() < (m - l - 1)){
-                ans = s.substr(l + 1, (m - l - 1));
+            if((end - start) < (m - l - 1)){
+                end = m;
+                start = l + 1;
+
             }
         }
 
-        return ans;
+        return s.substr(start, end - start);
 
     }
 
