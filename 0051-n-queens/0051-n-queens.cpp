8dd39fc2d0015d3337b300582e4e79f372a2425e
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-12-01 15:24:40 +0530

    Time: 3 ms (63.35%), Space: 10.2 MB (74.26%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-     void placeNQueens(int col, vector<string>& board, int n, vector<int>& leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal,vector<vector<string>>& ans){
-        if (col == n) {
+    
+    void solve(vector<vector<string>> &ans, vector<string> &board, vector<int> &lowerDiagonal, vector<int> &upperDiagonal, vector<int> &leftRow, int col, int n) {
+        if(col == n) {
             ans.push_back(board);
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 &&
-                upperDiagonal[n - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
+
+        for(int row = 0; row < n; row++) {
+            if(!upperDiagonal[(n - 1) + (col - row)] && !lowerDiagonal[row + col] && !leftRow[row]) {
+                upperDiagonal[(n - 1) + (col - row)] = 1;
                 lowerDiagonal[row + col] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
-                placeNQueens(col + 1, board, n, leftRow, upperDiagonal, lowerDiagonal, ans);
-                board[row][col] = '.';
-                leftRow[row] = 0;
+                leftRow[row] = 1;
+                board[row][col] = 'Q';
+                solve(ans, board, lowerDiagonal, upperDiagonal, leftRow, col + 1, n);
+                upperDiagonal[(n - 1) + (col - row)] = 0;
                 lowerDiagonal[row + col] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
+                leftRow[row] = 0;
+                board[row][col] = '.';
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n, string(n, '.'));
-        vector<int> leftRow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
-        placeNQueens(0, board, n, leftRow, upperDiagonal, lowerDiagonal, ans);
+        vector<string> board(n);
+        string s(n, '.');
+        for(int i = 0; i < n; i++) {
+            board[i] = s;
+        }
+        vector<int> lowerDiagonal(2*n - 1, 0);
+        vector<int> upperDiagonal(2*n - 1, 0);
+        vector<int> leftRow(n, 0);
+
+        solve(ans, board, lowerDiagonal, upperDiagonal, leftRow, 0, n);
         return ans;
     }
 };
