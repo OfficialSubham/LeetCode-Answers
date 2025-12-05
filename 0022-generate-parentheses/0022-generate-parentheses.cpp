f03ee0a88cac9119fcfed99fe9b2ec9c9e612d90
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-12-05 12:16:37 +0530

    Time: 3 ms (71.88%), Space: 15.6 MB (50.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //26/11/25
+/*
     void solve(string currString, int size, int depth, int currPair, vector<string> &ans) {
         if(currString.size() == size) {
             ans.push_back(currString);
@@ -22,4 +24,27 @@ public:
         solve("(", 2*n, 1, 0, ans);
         return ans;
     }
+*/
+
+void parenthesis(int n, string st, int p, int depth, vector<string> &ans) {
+    if(st.size() == n*2) {
+        ans.push_back(st);
+        return;
+    }
+    if(depth + p == n) {
+        parenthesis(n, st + ')', p+1, depth - 1, ans);
+    }
+    else {
+        parenthesis(n, st + '(', p, depth + 1, ans);
+        if(depth > 0) parenthesis(n, st + ')', p + 1, depth - 1, ans);
+    }
+
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    parenthesis(n, "(", 0, 1, ans);
+    return ans;
+}
+
 };
