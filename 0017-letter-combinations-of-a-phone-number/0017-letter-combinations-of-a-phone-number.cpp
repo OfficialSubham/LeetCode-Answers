53f9e76c389bf70a23cda327a122f0fd6d27c572
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-12-08 10:52:37 +0530

    Time: 0 ms (100%), Space: 9.3 MB (53.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-
-    void solve(vector<string> &ans, string digits, map<int, string> &hashMap, string eachAns, int idx) {
+    //29/11/25
+    void solve(vector<string> &ans, string digits, vector<string> &hashMap, string eachAns, int idx) {
         if(eachAns.size() == digits.size()) {
             ans.push_back(eachAns);
             return;
         }
-        string eachString = hashMap[digits[idx] - '0'];
+        int index = digits[idx] - '0';
+        string eachString = hashMap[index];
         for(int i = 0; i < eachString.size(); i++) {
             solve(ans, digits, hashMap, eachAns + eachString[i], idx + 1);
         } 
     }
 
     vector<string> letterCombinations(string digits) {
-        map<int, string> hashMap;
-        hashMap[2] = "abc";
-        hashMap[3] = "def";
-        hashMap[4] = "ghi";
-        hashMap[5] = "jkl";
-        hashMap[6] = "mno";
-        hashMap[7] = "pqrs";
-        hashMap[8] = "tuv";
-        hashMap[9] = "wxyz";
+        vector<string> hashMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> ans;
-        if(digits.size() == 1) {
-            string ansString = hashMap[digits[0] - '0'];
-            for(int i = 0; i < ansString.size(); i++) {
-                string eachLetter = "";
-                eachLetter += ansString[i];
-                ans.push_back(eachLetter);
-            }
-            return ans;
-        }
         solve(ans, digits, hashMap, "", 0);
         return ans;
     }
