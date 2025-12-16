8651e6a1fb07987c5e1bcf98046620b7c417b8cb
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-12-16 16:03:43 +0530

    Time: 1 ms (43.36%), Space: 9.9 MB (80.72%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
+    //26/11/25
+    void generate(int idx, vector<int> &nums, vector<int> &current, vector<vector<int>> &ans) {
+        ans.push_back(current);
+        for(int i = idx; i < nums.size(); i++) {
+            current.push_back(nums[i]);
+            generate(i + 1, nums, current, ans);
+            current.pop_back();
+        }
+    }
+
+    void generateUsingBits(vector<int> &arr, vector<vector<int>> &ans) {
+        int n = 1 << arr.size();
+        for(int i = 0; i < n; i++) {
+            vector<int> temp;
+            for(int j = 0; j < arr.size(); j++) {
+                if((i >> j )&1) temp.push_back(arr[j]);
+            }
+            ans.push_back(temp);
+        }
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> everySubset;
-        int totalSubset = 1 << n;
-        for(int i = 0; i < totalSubset; i++) {
-            vector<int> subset;
-            for(int j = 0; j < n; j++) {
-                if((i >> j) & 1) {
-                    subset.push_back(nums[j]);
-                }
-            }
-            everySubset.push_back(subset);
-        }
-
+        // vector<int> temp;
+        // generate(0, nums, temp, everySubset);
+        generateUsingBits(nums, everySubset);
         return everySubset;
     }
 };
