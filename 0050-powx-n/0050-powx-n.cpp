d8a74ca93c7736ff27a105c27b3a4386a3c55b02
Subham Mondal <168335967+OfficialSubham@users.noreply.github.com>
2025-12-18 16:23:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-
+/*
+    //24/11/25
     double solve(double x, long long n) {
         if(n == 0) return 1;
         if(n < 0) {
@@ -20,4 +21,46 @@ public:
     double myPow(double x, int n) {
        return solve(x, n);
     }
+
+
+    double solve(double x, long long n) {
+        //basecase
+        if(n < 0) {
+            return 1/(solve(x, -n));
+        }
+        if(n == 0) return 1;
+        //even
+        if(n % 2 == 0) {
+            double ans = solve(x, n/2);
+            return ans * ans;
+        }
+        //odd
+        else {
+            double ans = x  * solve(x, n-1);
+            return ans;
+        }
+    }
+
+    double myPow(double x, int n) {
+        return solve(x, n);
+    }
+*/
+    double myPow(double x, int n) {
+        double ans = 1;
+        bool isNegative = n < 0 ? true : false;
+        n = abs(n);
+        while(n > 0) {
+            if(n%2 == 0) {
+                n = n/2;
+                x =  x*x;
+            }
+            else {
+                n = n - 1;
+                ans = ans * x;
+            }
+        } 
+        if(isNegative) return 1/ans;
+        return ans;
+    }
+
 };
