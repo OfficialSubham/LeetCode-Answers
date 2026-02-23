ca8d937f5a732ebba56a667eb0cacd1efff41af2
subham <168335967+OfficialSubham@users.noreply.github.com>
2026-02-23 20:11:05 +0530

    Time: 0 ms (100%), Space: 15.5 MB (61.91%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -10,54 +10,34 @@
  */
 class Solution {
 public:
-    // ListNode* oddEvenList(ListNode* head) {
-        
-    //     int cnt = 0;
-    //     ListNode* mover = head;
-    //     //Handling Edge case
-    //     while(mover){
-    //         cnt++;
-    //         if(cnt > 2) break;
-    //         mover = mover->next;
-    //     }
-
-    //     if(cnt <= 2) return head;
-
-    //     ListNode* oddHead = head;
-    //     ListNode* evenHead = head->next;
-
-    //     ListNode* odd = oddHead;
-    //     ListNode* even = evenHead;
-
-    //     while(odd->next != nullptr && even->next != nullptr) {
-    //         odd->next = even->next;
-    //         odd = odd->next;
-    //         even->next = odd->next;
-    //         even = even->next;
-    //     }
-
-    //     odd->next = evenHead;
-
-    //     return head;
-    // }
-
     ListNode* oddEvenList(ListNode* head) {
         if(!head || !head->next) return head;
-        ListNode* oddNode = head;
-        ListNode* evenNode = head->next;
 
-        ListNode* oddTail = oddNode;
-        ListNode* evenTail = evenNode;
-        
-        while(oddTail->next && evenTail->next) {
-            oddTail->next = evenTail->next;
-            oddTail = oddTail->next;
-            evenTail->next = oddTail->next;
-            evenTail = evenTail->next;
+        ListNode* odd = new ListNode(0);
+        ListNode* oddMover = odd;
+        ListNode* even = new ListNode(0);
+        ListNode* evenMover = even;
+
+        bool takeOdd = true;
+
+        while(head) {
+            ListNode* nextPtr = head->next;
+            if(takeOdd) {
+                oddMover->next = head;
+                oddMover = head;
+                oddMover->next = nullptr;
+            }
+            else {
+                evenMover->next = head;
+                evenMover = head;
+                evenMover->next = nullptr;
+            }
+
+            head = nextPtr;
+            takeOdd = !takeOdd;
         }
-        oddTail->next = evenNode;
-        return oddNode;
 
+        oddMover->next = even->next;
+        return odd->next;
     }
-
 };
