0e334f72ed6cc812946cf64220f81f5fb0ac1303
subham <168335967+OfficialSubham@users.noreply.github.com>
2026-02-24 15:50:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -10,64 +10,26 @@
  */
 class Solution {
 public:
-    // ListNode* deleteMiddle(ListNode* head) {
-    //     int cnt = 0;
-    //     ListNode* mover = head;
-    //     while(mover) {
-    //         cnt++;
-    //         mover = mover->next;
-    //     }
-
-    //     if(cnt == 1) {
-    //         return head->next;
-    //     }
-    //     else if(cnt == 2) {
-    //         head->next = nullptr;
-    //         return head;
-    //     }
-
-    //     int middle = cnt/2;
-    //     int ptr = 0;
-    //     mover = head;
-    //     ListNode* temp = head;
-    //     while(mover) {
-    //         if(ptr == middle) {
-    //             temp->next = mover->next;
-    //             delete mover;
-    //             return head;
-    //         }
-    //         ptr++;
-    //         temp = mover;
-    //         mover = mover->next;
-    //     }
-
-    //     return head;
-
-    // }
-
     ListNode* deleteMiddle(ListNode* head) {
-        if(!head || !head->next) return nullptr;
-
         int len = 0;
         ListNode* mover = head;
+
         while(mover) {
             len++;
             mover = mover->next;
         }
 
         int middle = len/2;
-
         mover = head;
-
-        while(middle--) {
+        while(mover && middle--) {
             if(!middle) {
-                ListNode* next = mover->next;
-                mover->next = next->next;
-                return head;
+                ListNode* temp = mover->next;
+                if(temp) mover->next = temp->next;
+                break;
             }
             mover = mover->next;
         }
         return head;
-    }
 
+    }
 };
