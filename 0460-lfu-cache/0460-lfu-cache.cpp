d17b1d853d1fe40fe54ad0bcf04ae005de729f09
subham <168335967+OfficialSubham@users.noreply.github.com>
2026-03-30 16:01:32 +0530

    Time: 161 ms (20.14%), Space: 211 MB (9.8%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,112 +1,112 @@
-class Node {
+class DLL {
     public:
-    int key, value, cnt;
-    Node* next;
-    Node* prev;
+    int value, freq, key;
+    DLL* next;
+    DLL* prev;
 
-    Node(int k, int v): key(k), value(v), cnt(1), next(nullptr), prev(nullptr) {}
+    DLL(int v, int k):  value(v), freq(1), key(k), next(nullptr), prev(nullptr) {};
 };
 
-class List {
+class LRUCache {
     public:
     int size;
-    Node* head;
-    Node* tail;
+    DLL* head;
+    DLL* tail;
     
-    List() {
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head->next = tail;
-        tail->prev = head;
+    LRUCache() {
+        this->head = new DLL(0, 0);
+        this->tail = new DLL(0, 0);
+        this->head->next = tail;
+        this->tail->prev = head;
         size = 0;
     }
 
-    void remove(Node* node) {
-        Node* prevNode = node->prev;
-        Node* nextNode = node->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
-        size--;
-    }
-
-    void addFront(Node* node) {
-        Node* temp = head->next;
+    void addToTail(DLL* node) {
+        DLL* temp = head->next;
         node->next = temp;
         node->prev = head;
         head->next = node;
         temp->prev = node;
-        size++;
+        this->size++;
     }
 
+     void removeNode(DLL* node) {
+        DLL* delPrev = node->prev;
+        DLL* delNext = node->next;
+        delPrev->next = delNext;
+        delNext->prev = delPrev;
+        this->size--;
+    }
 };
-
 
 class LFUCache {
 public:
+    int miniFreq, capacity, currSize;
+    unordered_map<int, DLL*> keyNodeMap;
+    unordered_map<int, LRUCache*> freqListMap;
 
-    map<int, Node*> keyNode;
-    map<int, List*> freqListMap;
-    int capacity;
-    int minFreq;
-    int currSize;
-    LFUCache(int cap) {
-        capacity = cap;
-        minFreq = 0;
+    void updateFreqList(DLL* node) {
+        keyNodeMap.erase(node->key);
+        freqListMap[node->freq]->removeNode(node);
+
+        if(node->freq == this->miniFreq && freqListMap[node->freq]->size == 0) {
+            this->miniFreq++;
+        }
+
+        LRUCache* nextHigherFreq = new LRUCache();
+        if(freqListMap.count(node->freq + 1)) {
+            nextHigherFreq = freqListMap[node->freq+1];
+        }
+
+        node->freq += 1;
+        nextHigherFreq->addToTail(node); 
+        freqListMap[node->freq] = nextHigherFreq;
+        keyNodeMap[node->key] = node;
+    }
+
+
+    LFUCache(int c) {
+        miniFreq = 0;
         currSize = 0;
+        capacity = c;
     }
     
-    void updateFreqListMap(Node* node) {
-        keyNode.erase(node->key);
-        freqListMap[node->cnt]->remove(node);
-        if(node->cnt == minFreq && freqListMap[node->cnt]->size == 0) {
-            minFreq++;
-        }
 
-        List* nextHigherListFreq = new List();
-        if(freqListMap.find(node->cnt + 1) != freqListMap.end()) {
-            nextHigherListFreq = freqListMap[node->cnt + 1];
-        }
-
-        node->cnt += 1;
-        nextHigherListFreq->addFront(node);
-        freqListMap[node->cnt] = nextHigherListFreq;
-        keyNode[node->key] = node;
-    }
 
     int get(int key) {
-        if(keyNode.find(key) != keyNode.end()) {
-            Node* node = keyNode[key];
+        if(keyNodeMap.count(key)) {
+            DLL* node = keyNodeMap[key];
             int val = node->value;
-            updateFreqListMap(node);
+            updateFreqList(node);
             return val;
         }
+
         return -1;
     }
     
     void put(int key, int value) {
-        if(capacity == 0) return;
-        if(keyNode.find(key) != keyNode.end()) {
-            Node* node = keyNode[key];
+        if(keyNodeMap.count(key)) {
+            DLL* node = keyNodeMap[key];
             node->value = value;
-            updateFreqListMap(node);
+            updateFreqList(node);
         }
         else {
             if(currSize == capacity) {
-                List* list = freqListMap[minFreq];
-                keyNode.erase(list->tail->prev->key);
-                freqListMap[minFreq]->remove(list->tail->prev);
+                LRUCache* list = freqListMap[this->miniFreq];
+                keyNodeMap.erase(list->tail->prev->key);
+                list->removeNode(list->tail->prev);
                 currSize--;
             }
             currSize++;
-            minFreq = 1;
-            List* listFreq = new List();
-            if(freqListMap.find(minFreq) != freqListMap.end()) {
-                listFreq = freqListMap[minFreq];
+            this->miniFreq = 1;
+            DLL* node = new DLL(value, key);
+            LRUCache* list = new LRUCache();
+            if(freqListMap.count(this->miniFreq)) {
+                list = freqListMap[this->miniFreq];
             }
-            Node* node = new Node(key, value);
-            listFreq->addFront(node);
-            keyNode[key] = node;
-            freqListMap[minFreq] = listFreq;
+            list->addToTail(node);
+            keyNodeMap[key] = node;
+            freqListMap[this->miniFreq] = list;
         }
     }
 };
